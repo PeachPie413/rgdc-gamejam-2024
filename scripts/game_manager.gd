407e5d53073befc2a7db6438650c627b4c2c802d
Rat Queen <eggcelent4@gmail.com>
2024-10-25 19:29:28 -0400

    created player movement

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -9,12 +9,16 @@ extends Node
 var menu_scene: PackedScene = load("res://scenes/main_menu.tscn")
 var game_scene: PackedScene = load("res://scenes/game_world.tscn")
 var game_over_scene: PackedScene = load("res://scenes/game_over.tscn")
+var jamie_scene: PackedScene = load("res://scenes/jamie_test_scene.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# load the main menu on game start
-	load_menu()
+	# load_menu()
+	
+	# load jamie's test scene (comment this out)
+	load_jamie_scene()
 
 
 # Loads the mani menu scene, removing any other loaded scene
@@ -33,6 +37,13 @@ func load_game() -> void:
 func load_game_over() -> void:
 	remove_all_children()
 	add_child(game_over_scene.instantiate())
+
+
+# Loads my test scene, dont use this scene unless you are me and testing stuff
+# - Jamie
+func load_jamie_scene() -> void:
+	remove_all_children()
+	add_child(jamie_scene.instantiate())
 
 
 # Removes all children of the node
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,20 +1,49 @@
 extends CharacterBody2D
 
 
+# Move speed of the player in the horizontal direction
+@export var horizontal_move_speed: float
+
+# maximum speed the player can fall (terminal velocity)
+@export var terminal_velocity: float
+
+# acceleration of the player when they are going up
+@export var upwards_gravity: float
+
+# acceleraton of the player when they are going down
+@export var downwards_velocity: float
+
+# how fast the player jumps up (their initial velocity)
+@export var jump_force: float
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
-	set_player_velocity()
+func _physics_process(delta: float) -> void:
+	set_player_velocity(delta)
 	move_and_slide()
 
 
-
-
-
 # sets the velocity of the player
-func set_player_velocity() -> void:
-	pass
+func set_player_velocity(delta: float) -> void:
+	
+	# get horizontal input of player
+	var horizontal_input = Input.get_axis("move_left", "move_right")
+	velocity.x = horizontal_input * horizontal_move_speed
+	
+	# have the player fall
+	if velocity.y < 0:
+		velocity.y += upwards_gravity * delta
+	else:
+		velocity.y += downwards_velocity * delta
+	velocity.y = max(velocity.y, -terminal_velocity)
+	
+	# jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
