4d53ff6dd6a74813f45e35a2e7330f7b2862b93d
Rat Queen <eggcelent4@gmail.com>
2024-10-26 16:23:02 -0400

    worked on main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,16 +4,13 @@ class_name MainMenu extends Control
 signal game_started
 
 
-func start():
+func start_game():
 	game_started.emit()
-	print(":3")
 	pass
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var btn = get_node("Play Button") # or create a button scene instead
-	btn.pressed.connect(start)
+func quit_game():
+	get_tree().quit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
