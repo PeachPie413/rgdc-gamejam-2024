a7b5e43a611317e8d6cd5c63b81349166953ecfc
WalrusesAreAwesome <rysdam.jon@gmail.com>
2024-10-27 13:56:57 -0400

    Music :3

---
+++ scripts/audio_listener_2d.gd
@@ -0,0 +1,15 @@
+extends AudioStreamPlayer2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_level_manager_to_win_screen() -> void:
+	pass # Replace with function body.
